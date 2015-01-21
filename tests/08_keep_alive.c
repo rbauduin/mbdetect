5fae62c57235141823bfea7f51d24d0166f6d09b
Bauduin Raphaël <raphael.bauduin@uclouvain.be>
2015-01-21 11:36:04 +0100

    start of http keep-alive test

---
+++ tests/08_keep_alive.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <curl/curl.h>
+ 
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+ 
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8080");
+    /* example.com is redirected, so we tell libcurl to follow redirection */ 
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    /* also print headers */
+    curl_easy_setopt(curl, CURLOPT_HEADER, 1L );
+
+
+    // FIXME
+    // Default is already keep-alive
+    // Maybe test it with Connection: close
+    // Set headers
+    struct curl_slist *headers=NULL; 
+    headers = curl_slist_append(headers, "Connection: Keep-Alive"); 
+    res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers); 
+
+
+ 
+    /* Perform the request, res will get the return code */ 
+    res = curl_easy_perform(curl);
+    /* Check for errors */ 
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* always cleanup */ 
+    curl_easy_cleanup(curl);
+  }
+  return 0;
+}
