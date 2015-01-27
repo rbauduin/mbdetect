5a088708d44cc79536e9f37b0b22cfb8b27490ca
Bauduin Raphaël <raphael.bauduin@uclouvain.be>
2015-01-27 14:34:57 +0100

    added ftp

---
+++ tests/14_ftp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <curl/curl.h>
+ 
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+ 
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "ftp://ftp.belnet.be/mirror/HEADER.html");
+ 
+    /* Perform the request, res will get the return code */ 
+    res = curl_easy_perform(curl);
+    /* Check for errors */ 
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* always cleanup */ 
+    curl_easy_cleanup(curl);
+  }
+  return 0;
+}
