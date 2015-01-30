847b4a59c08b4e7fd530e48348a3f7d77763b622
Bauduin Raphaël <raphael.bauduin@uclouvain.be>
2015-01-30 14:30:28 +0100

    file output ok

--- tests/generic_runner.c
+++ tests/generic_runner.c
@@ -13,18 +13,49 @@ typedef struct {
 mapping mappings[] =  {
 	{"CURLOPT_URL", CURLOPT_URL, "str"},
 	{"CURLOPT_HEADER", CURLOPT_HEADER, "long"},
-	{"CURLOPT_FOLLOWLOCATION", CURLOPT_FOLLOWLOCATION, "long"}
+	{"CURLOPT_FOLLOWLOCATION", CURLOPT_FOLLOWLOCATION, "long"},
+	{"CURLOPT_POST", CURLOPT_POST, "long"},
+	{"CURLOPT_POSTFIELDSIZE", CURLOPT_POSTFIELDSIZE, "long"},
+	{"CURLOPT_POSTFIELDS",CURLOPT_POSTFIELDS,"str"}
+
 }; 
 
 int mappings_len = sizeof(mappings)/sizeof(mappings[0]);
 
+
+ 
+// structure keeping where we write
+typedef struct write_dest {
+	FILE *fd;
+	size_t size;
+} file_write;
+
+static size_t discard_data(void *ptr, size_t size, size_t nmemb, void *userp){
+  return size*nmemb;
+}
+
+static size_t
+write_in_file(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  file_write *dest;
+  size_t realsize = size * nmemb, written;
+  dest = (file_write *)userp;
+  written = fwrite(contents, size, nmemb, dest->fd);
+  return written*size;
+}
+
+
+
+
+
 int find_mapping(const char* option, mapping* m) {
 	int i=0;
 	mapping found;
-	while (i<mappings_len && strncmp(mappings[i].name,option, strlen(option))) {
+	while (i<mappings_len && strcmp(mappings[i].name, option)) {
 		i++;
 	}
 	if (i<mappings_len) {
+		//printf("mapping %s = option %s\n", mappings[i].name, option);
 		*m = mappings[i];
 		return 0;
 	}
@@ -37,7 +68,7 @@ int find_mapping(const char* option, mapping* m) {
  
 int find_code(const char* option) {
 	int i=0;
-	while (i<mappings_len && strncmp(mappings[i].name,option, strlen(option))) {
+	while (i<mappings_len && strcmp(mappings[i].name,option)) {
 		i++;
 	}
 	if (i<mappings_len) {
@@ -80,14 +111,15 @@ void set_options(CURL* curl, config_setting_t *test){
 			printf("ERROR, no mapping found!\n");
 			exit(1);
 		}
+		//printf("mapping : %s %s\n",m.name,m.type);
 		if (!strncmp(m.type,"str",3)) {
 			const char *value_str = config_setting_get_string(value);
-			printf("%s = %s\n", name_str, value_str);
+		//	printf("STR %s = %s\n", name_str, value_str);
 			curl_easy_setopt(curl, find_code(name_str), value_str); 
 		}
 		else if (!strncmp(m.type,"long",4)){
 			long value_long = config_setting_get_int64(value);
-			printf("%s = %lu\n", name_str, value_long);
+		//	printf("LONG %s = %lu\n", name_str, value_long);
 			curl_easy_setopt(curl, find_code(name_str), value_long); 
 		}
 		else {
@@ -109,6 +141,35 @@ void set_headers(CURL* curl, config_setting_t *test, struct curl_slist* headers)
     	res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers); 
 }
 
+void set_output(CURL* curl, config_setting_t *test, void *userp){
+	config_setting_t *output_file = config_setting_get_member(test, "output_file");
+	if (output_file==NULL){
+	    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, discard_data);
+	}
+	else {
+		file_write *dest = (file_write *) userp;
+		const char *path = config_setting_get_string(output_file);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_in_file);
+		FILE *f = fopen(path,"w");
+		dest->fd=f;
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, userp);
+	}
+}
+
+void clean_output(config_setting_t *test, void *userp){
+	config_setting_t *output_file = config_setting_get_member(test, "output_file");
+	if (output_file==NULL){
+	    return;
+	}
+	else {
+		file_write *dest = (file_write *) userp;
+		fclose(dest->fd);	
+		// FIXME: reset user structure to empty
+		dest->fd=NULL;
+		dest->size=0;
+	}
+}
+
 #define add_mapping(tab,code,type) tab[(code)] = (mapping) {#code, code, type} 
 int main(void)
 {
@@ -117,7 +178,7 @@ int main(void)
   config_t cfg;
   int ret,i,j,count;
  
-  ret = read_config("curl_tests.cfg", &cfg);
+  ret = read_config("one_test.cfg", &cfg);
   if(curl && ret==0) {
     config_setting_t *tests = config_lookup(&cfg, "tests");
     if(tests != NULL) {
@@ -127,19 +188,27 @@ int main(void)
       printf("found %d tests\n", count);
     }
     for (i=0; i<count; i++){
+	    file_write dest;
 	    curl = curl_easy_init();
 	    config_setting_t *test = config_setting_get_elem(tests, i);
 	    set_options(curl, test);
+            set_output(curl, test, &dest);
 	    struct curl_slist * curl_headers=NULL;
 	    set_headers(curl, test, curl_headers);
 
 	    /* Perform the request, res will get the return code */ 
 	    res = curl_easy_perform(curl);
 	    /* Check for errors */ 
+	    double content_len;
 	    if(res != CURLE_OK)
 		    fprintf(stderr, "curl_easy_perform() failed: %s\n",
 				    curl_easy_strerror(res));
+	    else {
+		    curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &content_len);
+		    printf("Content length was : %f\n", content_len);
+	    }
 	    /* always cleanup */ 
+	    clean_output(test, &dest);
 	    curl_slist_free_all(curl_headers);
 	    curl_easy_cleanup(curl);
     }
